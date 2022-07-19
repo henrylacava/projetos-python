33134c78ba398322627d2129ea802cdd3671adad
Henry <109253086+henrylacava@users.noreply.github.com>
2022-07-19 03:07:22 -0300

    Update pedra papel ou tesoura.py

--- pedra papel ou tesoura.py
+++ pedra papel ou tesoura.py
@@ -1,15 +1,16 @@
 while True:
     vc = str(input('Pedra, Papel ou Tesoura? \n'))
-    if vc == 'Pedra':
+    if vc == 'Pedra' or vc == 'pedra':
         print('Você escolheu Pedra')
         break
-    elif vc == 'Papel':
+    elif vc == 'Papel' or vc == 'papel':
         print('Você escolheu Papel')
         break
-    elif vc == 'Tesoura':
+    elif vc == 'Tesoura' or vc == 'tesoura':
         print('Você escolheu Tesoura')
         break
-
+    
+#escolha pc
 import random
 
 pc = random.randrange(1,4)
@@ -40,4 +41,25 @@ elif pc == 1 and vc == 'Papel':
 elif pc == 2 and vc == 'Tesoura':
     print('\n Você ganhou!')
 elif pc == 3 and vc == 'Pedra':
+    print('\n Você ganhou!')
+#============================================#
+if pc == 1 and vc == 'pedra':
+    print('\n Empate')
+elif pc == 2 and vc == 'papel':
+    print('\n Empate')
+elif pc == 3 and vc == 'tesoura':
+    print('\n Empate')
+
+elif pc == 1 and vc == 'tesoura':
+    print('\n Você perdeu')
+elif pc == 2 and vc == 'pedra':
+    print('\n Você perdeu')
+elif pc == 3 and vc == 'papel':
+    print('\n Você perdeu')
+    
+elif pc == 1 and vc == 'papel':
+    print('\n Você ganhou!')
+elif pc == 2 and vc == 'tesoura':
+    print('\n Você ganhou!')
+elif pc == 3 and vc == 'pedra':
     print('\n Você ganhou!')
